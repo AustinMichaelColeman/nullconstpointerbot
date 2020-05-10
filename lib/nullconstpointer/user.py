2bb0c331a35498783d48ecf573c0e49dfeb2227b
Austin Coleman <austinmichaelcoleman@gmail.com>
2020-05-10 14:54:42 -0700

    Added start to user class and tests

--- lib/nullconstpointer/user.py
+++ lib/nullconstpointer/user.py
@@ -1,7 +1,27 @@
+MOD_LEVEL_OWNER = 0
+MOD_LEVEL_MOD = 1
+MOD_LEVEL_USER = 2
+
+
 class User:
-    def __init__(self, name, level):
+    def __init__(self, name, level, modlevel=MOD_LEVEL_USER):
         self.name = name
         self.levels = [level]
+        self.modlevel = modlevel
 
     def levelCount(self):
         return len(self.levels)
+
+    def next_level(self):
+        if len(self.levels) > 0:
+            return self.levels[0]
+        return None
+
+    def remove_level(self):
+        self.levels.pop(0)
+
+    def add_level(self, level):
+        self.levels.append(level)
+
+    def mod_level(self):
+        return self.modlevel
--- test_users.py
+++ test_users.py
@@ -1,12 +1,52 @@
 import unittest
-from lib.nullconstpointer import user
+from lib.nullconstpointer import user, level
 
 
 class TestUsers(unittest.TestCase):
-    def test_user_level_count_empty(self):
+    def test_user_mod_level_is_user_by_default(self):
         theuser = user.User("userA", "xxx-xxx-xxx")
+        self.assertEqual(theuser.modlevel, user.MOD_LEVEL_USER)
 
+    def test_user_level_count_is_one_by_default(self):
+        theuser = user.User("userA", "xxx-xxx-xxx")
         self.assertEqual(theuser.levelCount(), 1)
+
+    def test_user_level_is_formatted(self):
+        thelevel = level.Level("xxx-xxx-xxx")
+        theuser = user.User("userA", thelevel)
+        self.assertEqual(theuser.next_level().code(), "XXX-XXX-XXX")
+
+    def test_user_can_remove_level(self):
+        thelevel = level.Level("xxx-xxx-xxx")
+        theuser = user.User("userA", thelevel)
+        theuser.remove_level()
+        self.assertEqual(theuser.next_level(), None)
+
+    def test_users_can_have_multiple_levels(self):
+        levelOne = level.Level("abc-def-ghd")
+        theuser = user.User("userA", levelOne)
+        levelTwo = level.Level("aaa-bbb-ccc")
+        theuser.add_level(levelTwo)
+
+        self.assertEqual(theuser.next_level().code(), levelOne.code())
+        theuser.remove_level()
+        self.assertEqual(theuser.next_level().code(), levelTwo.code())
+        theuser.remove_level()
+        self.assertEqual(theuser.next_level(), None)
+
+    def test_user_can_be_mod_level_mod(self):
+        theuser = user.User("moderator", level.Level("abc-abc-abc"), user.MOD_LEVEL_MOD)
+        self.assertEqual(theuser.mod_level(), user.MOD_LEVEL_MOD)
+
+    def test_user_can_be_mod_level_owner(self):
+        thelevel = level.Level("abc-abc-abc")
+        theuser = user.User("moderator", thelevel, user.MOD_LEVEL_OWNER)
+        self.assertEqual(theuser.mod_level(), user.MOD_LEVEL_OWNER)
+
+    def test_user_can_be_mod_level_user(self):
+        thelevel = level.Level("abc-abc-abc")
+        theuser = user.User("moderator", thelevel, user.MOD_LEVEL_USER)
+        self.assertEqual(theuser.mod_level(), user.MOD_LEVEL_USER)
 
 
 if __name__ == "__main__":
