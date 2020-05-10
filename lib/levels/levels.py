4a78d46d55ae93812f692d55d2c7813013cc9e64
Austin Coleman <austinmichaelcoleman@gmail.com>
2020-05-09 22:27:17 -0700

    initial commit of levels

---
+++ lib/levels/levels.py
@@ -0,0 +1,6 @@
+class Levels:
+    def __init__(self):
+        self.levels = []
+
+    def empty(self):
+        return len(self.levels) == 0
--- test_stringmethods.py
+++ test_stringmethods.py
@@ -1,20 +1,12 @@
 import unittest
+from lib.levels import levels
 
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+class TestUserLevelQueue(unittest.TestCase):
+    def test_level_queue_empty(self):
+        levels_instance = levels.Levels()
 
-    def test_isupper(self):
-        self.assertTrue("FOO".isupper())
-        self.assertFalse("Foo".isupper())
-
-    def test_split(self):
-        s = "hello world"
-        self.assertEqual(s.split(), ["hello", "world"])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        self.assertEqual(levels_instance.empty(), True)
 
 
 if __name__ == "__main__":
