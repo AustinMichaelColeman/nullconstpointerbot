cc324af7a28821eb4551b6775f8d07edabbf4075
Austin Coleman <austinmichaelcoleman@gmail.com>
2020-05-10 13:46:42 -0700

    Added level code validation

--- lib/nullconstpointer/level.py
+++ lib/nullconstpointer/level.py
@@ -1,6 +1,28 @@
+import re
+
+
 class Level:
-    def __init__(self):
-        self.level_code = ""
+    def __init__(self, level):
+        self.level_code = self.validate(level)
 
     def code(self):
         return self.level_code
+
+    def validate(self, level):
+        # Text allowed in level codes: 0-9 A-Z a-z except I O Z
+        # ignore anything but 0-9, A-Z, a-z, except IiOoZz
+        # remove everything but 0-9, A-Z, a-z except IiOoZz
+        non_alpha_numberic_removed = re.sub("[\\W_IiOoZz]+", "", level)
+        if len(non_alpha_numberic_removed) != 9:
+            return ""
+        non_alpha_numberic_removed = non_alpha_numberic_removed.upper()
+
+        validated_level = (
+            non_alpha_numberic_removed[0:3]
+            + "-"
+            + non_alpha_numberic_removed[3:6]
+            + "-"
+            + non_alpha_numberic_removed[6:9]
+        )
+
+        return validated_level
--- test_levels.py
+++ test_levels.py
@@ -4,9 +4,37 @@ from lib.nullconstpointer import level
 
 class TestLevels(unittest.TestCase):
     def test_level_code_empty(self):
-        levelinst = level.Level()
+        levelinst = level.Level("")
 
         self.assertEqual(levelinst.code(), "")
+
+    def test_invalid_level_is_empty(self):
+        # Text allowed in level codes: 0-9 A-Z a-z except I O Z
+        # ignore anything but 0-9, A-Z, a-z, except IiOoZz
+        # ensure level length
+        levelinst = level.Level("invalid_LEVEL_code0123456789-=}||test")
+        self.assertEqual(levelinst.code(), "")
+
+        # cannot contain iIOoZz
+        levelinst = level.Level("iIo-OzZ-abc")
+        self.assertEqual(levelinst.code(), "")
+
+        # must match correct length
+        levelinst = level.Level("abc-def-gh")
+        self.assertEqual(levelinst.code(), "")
+
+        levelinst = level.Level("abc-def-ghhh")
+        self.assertEqual(levelinst.code(), "")
+
+    def test_valid_level_is_capitalized(self):
+        valid_level = "abc-def-ghd"
+        levelinst = level.Level(valid_level)
+        self.assertEqual(levelinst.code(), "ABC-DEF-GHD")
+
+    def test_valid_level_ignores_whitespace(self):
+        valid_level = " abc ---_ def     ghd -]{}/"
+        levelinst = level.Level(valid_level)
+        self.assertEqual(levelinst.code(), "ABC-DEF-GHD")
 
 
 if __name__ == "__main__":
