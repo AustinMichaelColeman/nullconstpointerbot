10766ea78026bd6b603a5407a93b21d0315faa23
Austin Coleman <austinmichaelcoleman@gmail.com>
2020-05-10 22:10:05 -0700

    split Bot class from main to bot.py

---
+++ lib/nullconstpointer/bot.py
@@ -0,0 +1,99 @@
+"""
+Some code in this file is licensed under the Apache License, Version 2.0.
+    http://aws.amazon.com/apache2.0/
+"""
+from irc.bot import SingleServerIRCBot
+from requests import get
+from . import processor, user
+
+
+class Bot(SingleServerIRCBot):
+    def __init__(self, botname, owner, client_id, token):
+        self.HOST = "irc.chat.twitch.tv"
+        self.PORT = 6667
+        self.USERNAME = botname.lower()
+        self.CLIENT_ID = str(client_id)
+        self.TOKEN = str(token)
+        self.CHANNEL = f"#{owner}"
+        self.botname = botname
+        self.prefix = "!"
+
+        self.cmds = {
+            "hello": self.hello,
+            "friendcode": self.friendcode,
+            "add": self.add,
+            "github": self.github,
+        }
+
+        url = f"https://api.twitch.tv/kraken/users?login={self.USERNAME}"
+        headers = {
+            "Client-ID": self.CLIENT_ID,
+            "Accept": "application/vnd.twitchtv.v5+json",
+        }
+        resp = get(url, headers=headers).json()
+        self.channel_id = resp["users"][0]["_id"]
+
+        super().__init__(
+            [(self.HOST, self.PORT, f"oauth:{self.TOKEN}")],
+            self.USERNAME,
+            self.USERNAME,
+        )
+
+    def on_welcome(self, cxn, event):
+        for req in ("membership", "tags", "commands"):
+            cxn.cap("REQ", f":twitch.tv/{req}")
+
+        cxn.join(self.CHANNEL)
+        self.send_message("Now online.")
+
+    def on_pubmsg(self, cxn, event):
+        tags = {kvpair["key"]: kvpair["value"] for kvpair in event.tags}
+        user = {"name": tags["display-name"], "id": tags["user-id"]}
+        message = event.arguments[0]
+
+        if user["name"] != self.botname:
+            self.process(user, message)
+
+        print(f"Message from {user['name']}: {message}")
+
+    def send_message(self, message):
+        self.connection.privmsg(self.CHANNEL, message)
+
+    def process(self, user, message):
+        if message.startswith(self.prefix):
+            cmd = message.split(" ")[0][len(self.prefix) :]
+            args = message.split(" ")[1:]
+            self.perform(user, cmd, *args)
+
+    def perform(self, user, cmd, *args):
+        for name, func in self.cmds.items():
+            if cmd == name:
+                func(user, *args)
+                return
+
+        if cmd == "help":
+            self.help(self.prefix, self.cmds)
+
+        # else:
+        #     bot.send_message(f"{user['name']}, \"{cmd}\" isn't a registered command.")
+
+    def help(self, prefix, cmds):
+        self.send_message(
+            "Registered commands: "
+            + ",".join([f"{prefix}{cmd}" for cmd in sorted(cmds.keys())])
+        )
+
+    def hello(self, chatuser, *args):
+        self.send_message(f"Hey {chatuser['name']}!")
+
+    def friendcode(self, chatuser, *args):
+        self.send_message(f"Add me on your switch! My friend code is SW-2444-3895-1309")
+
+    def add(self, chatuser, *args):
+        cmdprocessor = processor.Processor()
+        theuser = user.User(chatuser["name"], "xxx-xxx-xxx")
+        response = cmdprocessor.add_user(theuser)
+        self.send_message(response)
+
+    def github(self, chatuser, *args):
+        self.send_message("https://github.com/AustinMichaelColeman/nullconstpointerbot")
--- main.py
+++ main.py
@@ -1,61 +1,11 @@
-"""
-Some code in this file is licensed under the Apache License, Version 2.0.
-    http://aws.amazon.com/apache2.0/
-"""
-
-from irc.bot import SingleServerIRCBot
-from requests import get
-from lib import nullconstpointer
+from lib.nullconstpointer import bot
 import keys
-
-NAME = "nullconstpointerbot"
-OWNER = "nullconstpointer"
-
-
-class Bot(SingleServerIRCBot):
-    def __init__(self):
-        self.HOST = "irc.chat.twitch.tv"
-        self.PORT = 6667
-        self.USERNAME = NAME.lower()
-        self.CLIENT_ID = str(keys.CLIENT_ID)
-        self.TOKEN = str(keys.TOKEN)
-        self.CHANNEL = f"#{OWNER}"
-
-        url = f"https://api.twitch.tv/kraken/users?login={self.USERNAME}"
-        headers = {
-            "Client-ID": self.CLIENT_ID,
-            "Accept": "application/vnd.twitchtv.v5+json",
-        }
-        resp = get(url, headers=headers).json()
-        self.channel_id = resp["users"][0]["_id"]
-
-        super().__init__(
-            [(self.HOST, self.PORT, f"oauth:{self.TOKEN}")],
-            self.USERNAME,
-            self.USERNAME,
-        )
-
-    def on_welcome(self, cxn, event):
-        for req in ("membership", "tags", "commands"):
-            cxn.cap("REQ", f":twitch.tv/{req}")
-
-        cxn.join(self.CHANNEL)
-        self.send_message("Now online.")
-
-    def on_pubmsg(self, cxn, event):
-        tags = {kvpair["key"]: kvpair["value"] for kvpair in event.tags}
-        user = {"name": tags["display-name"], "id": tags["user-id"]}
-        message = event.arguments[0]
-
-        if user["name"] != NAME:
-            nullconstpointer.process(bot, user, message)
-
-        print(f"Message from {user['name']}: {message}")
-
-    def send_message(self, message):
-        self.connection.privmsg(self.CHANNEL, message)
 
 
 if __name__ == "__main__":
-    bot = Bot()
+    name = "nullconstpointerbot"
+    owner = "nullconstpointer"
+    client_id = keys.CLIENT_ID
+    token = keys.TOKEN
+    bot = bot.Bot(name, owner, client_id, token)
     bot.start()
