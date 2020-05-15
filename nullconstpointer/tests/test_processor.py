aa7bc96a6f6503b0d1d447422b46bd07b7c8f37d
Austin Coleman <austinmichaelcoleman@gmail.com>
2020-05-14 19:17:55 -0700

    Remove unused imports in test_processor

--- nullconstpointer/tests/test_processor.py
+++ nullconstpointer/tests/test_processor.py
@@ -2,12 +2,6 @@ import unittest
 
 from nullconstpointer.processor import Processor
 from nullconstpointer.user import User, MOD_LEVEL_OWNER, MOD_LEVEL_MOD, MOD_LEVEL_USER
-from nullconstpointer.level import Level
-from nullconstpointer.commands.add import AddCommand
-from nullconstpointer.commands.mod import ModCommand
-from nullconstpointer.commands.unmod import UnmodCommand
-from nullconstpointer.commands.remove import RemoveCommand
-from nullconstpointer.commands.random import RandomCommand
 
 
 class TestProcessor(unittest.TestCase):
